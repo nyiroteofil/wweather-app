c01c6839d53cc7997dc5bf3dbc98c42d691bbfc6
NyirőTeofil <portrider01234@gmail.com>
2022-11-17 21:12:20 +0100

    Added icons for the forecast and worked on the styling

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -29,45 +29,55 @@ async function fetchWeatherMain(city) {
         let dailyWeather = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&cnt=5&APPID=a280ad62e485531769c9814cbc6bdfc8`, {mode: 'cors'});
         let dailyWeatherJSON = await dailyWeather.json();
 
-        console.log(dailyWeatherJSON);
-
-        setForecastValues(weatherJSON);
-
         document.querySelector('#current-city').textContent = dailyWeatherJSON.name;
-        console.log(weatherJSON)
-        setBackgrounds(dailyWeatherJSON.weather[0].main, ".weather-area")
 
-        setDays(weatherJSON);
+        setBackgrounds(dailyWeatherJSON.weather[0].main, ".weather-area")
 
         document.querySelector('#humidity').textContent = dailyWeatherJSON.main.humidity + '%';
     
         document.querySelector('#pressure').textContent = dailyWeatherJSON.main.pressure + ' hPa'
+
+        console.log(dailyWeatherJSON);
+
+        setForecastIcons(weatherJSON);
+
+        setForecastValues(weatherJSON);
+
+        setTodayValues(weatherJSON);
     } catch (error) {
         console.log(error);
     };
 }
 
+const setForecastIcons = (Obj) => {
+    setIcon(Obj.list[0].weather[0].main, '#forecast-weather-1');
+    setIcon(Obj.list[1].weather[0].main, '#forecast-weather-2');
+    setIcon(Obj.list[2].weather[0].main, '#forecast-weather-3');
+    setIcon(Obj.list[3].weather[0].main, '#forecast-weather-4');
+    setIcon(Obj.list[4].weather[0].main, '#forecast-weather-5');
+}
+
 /**assigning the forecasted teperatures */
 const setForecastValues = Obj => {
-    document.querySelector('#day-1-max').textContent = Obj.list[0].main.temp_max;
-    document.querySelector('#day-1-avg').textContent = Obj.list[0].main.temp;
-    document.querySelector('#day-1-min').textContent = Obj.list[0].main.temp_min;
+    document.querySelector('#day-1-max').textContent = `${Math.round(Obj.list[0].main.temp_max)}` + "°" ;
+    document.querySelector('#day-1-avg').textContent = Math.round(Obj.list[0].main.temp) + "°";
+    document.querySelector('#day-1-min').textContent = Math.round(Obj.list[0].main.temp_min) + "°"; 
 
-    document.querySelector('#day-2-max').textContent = Obj.list[1].main.temp_max;
-    document.querySelector('#day-2-avg').textContent = Obj.list[1].main.temp;
-    document.querySelector('#day-2-min').textContent = Obj.list[1].main.temp_min;
+    document.querySelector('#day-2-max').textContent = Math.round(Obj.list[1].main.temp_max) + "°"; 
+    document.querySelector('#day-2-avg').textContent = Math.round(Obj.list[1].main.temp) + "°";
+    document.querySelector('#day-2-min').textContent = Math.round(Obj.list[1].main.temp_min) + "°"; 
 
-    document.querySelector('#day-3-max').textContent = Obj.list[2].main.temp_max;
-    document.querySelector('#day-3-avg').textContent = Obj.list[2].main.temp;
-    document.querySelector('#day-3-min').textContent = Obj.list[2].main.temp_min;
+    document.querySelector('#day-3-max').textContent = Math.round(Obj.list[2].main.temp_max) + "°"; 
+    document.querySelector('#day-3-avg').textContent = Math.round(Obj.list[2].main.temp) + "°";
+    document.querySelector('#day-3-min').textContent = Math.round(Obj.list[2].main.temp_min) + "°"; 
 
-    document.querySelector('#day-4-max').textContent = Obj.list[3].main.temp_max;
-    document.querySelector('#day-4-avg').textContent = Obj.list[3].main.temp;
-    document.querySelector('#day-4-min').textContent = Obj.list[3].main.temp_min;
+    document.querySelector('#day-4-max').textContent = Math.round(Obj.list[3].main.temp_max) + "°"; 
+    document.querySelector('#day-4-avg').textContent = Math.round(Obj.list[3].main.temp) + "°";
+    document.querySelector('#day-4-min').textContent = Math.round(Obj.list[3].main.temp_min) + "°"; 
 
-    document.querySelector('#day-5-max').textContent = Obj.list[4].main.temp_max;
-    document.querySelector('#day-5-avg').textContent = Obj.list[4].main.temp;
-    document.querySelector('#day-5-min').textContent = Obj.list[4].main.temp_min;
+    document.querySelector('#day-5-max').textContent = Math.round(Obj.list[4].main.temp_max) + "°"; 
+    document.querySelector('#day-5-avg').textContent = Math.round(Obj.list[4].main.temp) + "°";
+    document.querySelector('#day-5-min').textContent = Math.round(Obj.list[4].main.temp_min) + "°"; 
 }
 
 const setBackgrounds = (weather, domElement) => {
@@ -104,10 +114,10 @@ const setBackgrounds = (weather, domElement) => {
     }
 }
 
-const setDays = (weatherObj) => {  
-    document.querySelector('#today-temp-1').textContent = `${weatherObj.list[0].main.temp}°`;
+const setTodayValues = (weatherObj) => {  
+    document.querySelector('#today-temp-1').textContent = `${Math.round(weatherObj.list[0].main.temp)}°`;
     setIcon(weatherObj.list[0].weather[0].main, "#today-temp-icon")
-    document.querySelector('#today-feel-1').textContent = `${weatherObj.list[0].main.feels_like}°`
+    document.querySelector('#today-feel-1').textContent = `${Math.round(weatherObj.list[0].main.feels_like)}°`
 }
 
 const setIcon = (weather, DOMelement) => {
